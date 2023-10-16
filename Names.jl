f6bf6836594e20b3fe362369e0781e45d4937b15
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2023-10-16 18:17:47 +0800

    Names section start

---
+++ Names.jl
@@ -0,0 +1,17 @@
+#The aim of this part is to understand the use of variables and functions.
+
+#In julia, variable name is also called an identifier. An identifier may
+#include any upper, lower letters, any digit, one underscore_ and one exclamation!
+#But the first character in an identifier cannot be a digit or !.
+#In addition, any Unicode characters can be used in a variable name:
+α7= 0.7
+escape_velocity=9.8
+
+#A variable name with only of underscore_, can be a assigned a value and
+#discarded immediately and cannot be used in subsequent expression.
+
+#another special variable is ans, which takes the value of the most recent result
+# in a REPL. For example:
+8+3
+x = 3+ans
+
