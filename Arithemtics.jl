b975c9177c1850ee955f070b67737d715b89fac9
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2023-10-16 16:00:11 +0800

    Try on GH desktop

--- Arithemtics.jl
+++ Arithemtics.jl
@@ -3,3 +3,8 @@ typeof(64)
 #Use supertype() and subtype() to check for other possible types.
 supertype(Int64)
 subtypes(Real)
+#Julia uses Inf as infinite:
+typemax(Float64)
+#and NaN is used in Julia for "not a number", this means the calculation is meaninngless no matter finite or infinite.
+0.0/0.0
+
