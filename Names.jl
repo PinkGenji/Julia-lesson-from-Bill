bc44116cc90675ef163e7a3c27af584287c71479
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2023-10-16 21:27:22 +0800

    Naming, special constants and modules

--- Names.jl
+++ Names.jl
@@ -15,3 +15,46 @@ escape_velocity=9.8
 8+3
 x = 3+ans
 
+#Like Python, Julia uses +=, -=, *= and /= for updating a variable:
+x += x+1
+#A semicolon can be used to supress the evaluation in the REPL:
+x = 3*5.2e7;
+#Semicolon can also be used to split two statement on the same line:
+x = 5; y = x^2
+#Power operation has higher priority than usual operations, e.g. times
+2x^2
+(2x)^2
+2(x^2)
+
+#Julia also identifies many Unicode characters. Including all Greeks:
+λ=4
+ξ=5
+γ=6
+
+#Special constants are pre-defined in Julia, and some of them have their
+#own types: irrational{}, i.e.:
+typeof(pi)
+typeof(π)
+typeof(ℯ)
+#Indeed, they are special, even in Julia!
+π
+ℯ
+#Julia will round π to a floating point value based on the types of the
+#other variables and constants in the expression:
+π+Float32(5)
+#we can use big() to get the max precision representation:
+big(π)
+
+#Julia has a large standard library. We can use these modules to help:
+using Dates
+t = Date(1845,3,3)
+dayname(t)
+
+#If we use "import" like python, we will only import the module name
+#available, but not the names taht the module exports:
+import Dates
+t = Dates.Date(1956,6,19)
+Dates.dayname(t)
+#Although 'using' is helpful, a name clash can occur when you use multiple
+#modules. A programmer wanting to use both functions in the same program could 
+#use the "import" command to distinguish them.
