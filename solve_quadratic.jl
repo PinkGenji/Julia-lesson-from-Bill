7fbe92ace58c35325621319fe366f6adb9ca00e5
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2023-10-17 19:30:34 +0800

    Back up 'Functions'

--- solve_quadratic.jl
+++ solve_quadratic.jl
@@ -8,3 +8,15 @@ end
 #We say that 'sqrt_dscr, x_plus, x_minus' are local variables in the scoping unit of the function 'solve-quadratic'.
 #The local variables cannot be referenced outside their smallest enclosing scoping unit.
 
+#We do not need to define all values as local variables. We can leave the dummy variable as it is, when we do not know the valueL:
+h3(x) = 2x-b #here, b is a dummy variable that need to be defined in the 'global' scope.
+
+#When there is no return written, Julia just returns the value of the final line of its body:
+function h1(x)
+    a = 3
+    x^2+a
+end 
+
+#Anonymous Functions...
+
+
