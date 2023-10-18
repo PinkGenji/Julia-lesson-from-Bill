9c636c96ebd98a3da77cada75bc7aadc87ef6515
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2023-10-18 15:48:24 +0800

    plotting

--- Plotting.jl
+++ Plotting.jl
@@ -1,6 +1,23 @@
 #Make sure 'Plots' is installed.
 #Start with a simple plotting:
 using Plots
-x = range(0,2π, length = 201)
+x = range(0,2π, length = 201) #range() crates an AbstractVector object.
 y = sin.(x)
 plot(x,y)
+
+#We can modify the plot style:
+plot(x,y, linestyle=:dash)
+plot(x,y, linestyle=:dashdotdot)
+#The plotattr() is useful for looking up plot attributes:
+plotattr("linestyle")
+#We can use ls, s or style for linestyle:
+plot(x,y, ls=:dash)
+plot(x,y, linecolor= :red)
+
+#We can also plot discrete data using a variety of different marker symbols using the scatter function:
+x = range(1,5, length=9)
+y = exp.(-x/2) +2*sin.(40x);
+scatter(x,y, markershape = :circle,
+markercolor = :green)
+
+
