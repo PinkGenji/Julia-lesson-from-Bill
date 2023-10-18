25db42adc8e6316c7b8c7e836758332d2abeea6e
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2023-10-18 17:46:33 +0800

    Back up Plotting commands

--- Plotting.jl
+++ Plotting.jl
@@ -18,6 +18,31 @@ plot(x,y, linecolor= :red)
 x = range(1,5, length=9)
 y = exp.(-x/2) +2*sin.(40x);
 scatter(x,y, markershape = :circle,
-markercolor = :green)
+markercolor = :green, title = "The graph of scatter")
 
+#The Plots documentation uses the term series to describe a set of (x,y) data values. We can use
+#the plot!() function to add an additional series to an existing plot:
+x = range(0,π, length=201);
+y1 = sin.(3x);
+plot(x,y1, title = "Multiple series")
+y2 = cos.(2x.-π);
+plot!(x,y2)
+y3 = 0.5*sin.(2x.+π/4);
+plot!(x,y3)
+
+#To save a copy of a plot as .png or .pdf, we can use:
+#savefig("myplot.png") or savefig("myplot.pdf")
+
+#Subplots...
+#It is also possible to display several subplots, see the file "my_subplots.jl"
+
+#Axis tweaks...
+#We can also adjust the location and labels of the tick marks on an axis using the 'xticks' and 'yticks' keyword:
+x = range(0, 2π, length = 201);
+y = sin.(x);
+plot(x,y, legend = false, xticks = ([0, π/2, π, 3π/2, 2π], ["0", "π/2", "π", "3π/2", "2π"]))
+#We can also manually adjust the axis limits using 'xlims, ylims':
+plot(x,y, legend = false, xticks = ([0, π/2, π, 3π/2, 2π], ["0", "π/2", "π", "3π/2", "2π"]), ylims=(0, 1))
+
+#Polar plots...
 
