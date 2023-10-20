3a2979ccc167ea01f70b5f772fc0911ae5e7d63a
PinkGenji <63383516+PinkGenji@users.noreply.github.com>
2023-10-20 12:52:46 +0800

    Update Files.jl

--- Files/Files.jl
+++ Files/Files.jl
@@ -1,4 +1,5 @@
 #This aim of this lesson is to learn how to read from and write to files via Julia.
 
 #The text file quote.txt contains an extract from the book Progress and Poverty.
-
+#open the file by:
+io = open("quote.txt","r")
